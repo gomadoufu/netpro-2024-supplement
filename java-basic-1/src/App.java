f4b14981921dd57aa124e74e37371001d2f252d2
gomadoufu <49393637+gomadoufu@users.noreply.github.com>
2024-04-17 02:24:43 +0900

    initial commit ✨

---
+++ java-basic-1/src/App.java
@@ -0,0 +1,30 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        var integer = 10;
+        var doubleValue = 10.0;
+        var string = "Hello World";
+
+        if (integer == 10) {
+            System.out.println(doubleValue * 2);
+            System.out.println(string);
+        }
+
+        var app = new App();
+
+        System.out.println(formatterPatternSwitch(integer));
+        System.out.println(formatterPatternSwitch(doubleValue));
+        System.out.println(formatterPatternSwitch(string));
+        System.out.println(formatterPatternSwitch(app));
+
+    }
+
+    static String formatterPatternSwitch(Object obj) {
+        return switch (obj) {
+            case Integer i -> String.format("int %d", i);
+            case Double d -> String.format("double %f", d);
+            case String s -> String.format("String %s", s);
+            default -> obj.toString();
+        };
+    }
+
+}
